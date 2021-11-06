267df68ceec4aec8a9d093bd70cbfb2f9c1f9f4f
Jakob Jordan <jakobjordan@posteo.de>
2021-11-06 13:14:59 +0100

    Work on plot

--- plot.py
+++ plot.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 import numpy as np
 from scipy.interpolate import splrep, splev
+import sys
+
+sys.path.insert(0, './utils')
+from fit_GP_and_predict import fit_gp_and_predict
 
 
 colors_categories = {
@@ -25,13 +29,14 @@ if __name__ == '__main__':
 
     start_year = 2008
     end_year = 2022
+    extrapolate_year = 2030
 
     n_years = end_year - start_year
     n_months = n_years * 12
     time_years = np.arange(n_years)
     time_months = np.arange(n_months)
     sdg_spending_0 = 10 + 0.05 * time_months + 0.3 * np.random.randn(n_months)
-    sdg_spending_1 = 9 - 0.005 * time_months + 0.3 * np.random.randn(n_months)
+    sdg_spending_1 = 9 - 0.015 * time_months + 0.3 * np.random.randn(n_months)
     sdg_spending_2 = 11 + 0.05 * time_months + 0.3 * np.random.randn(n_months)
     sdg_spending_3 = 13 - 0.05 * time_months + 0.3 * np.random.randn(n_months)
     total_spending = 80 + 0.06 * time_months + 0.2 * np.random.randn(n_months)
@@ -41,22 +46,25 @@ if __name__ == '__main__':
     measure_three = 20 - 1.05 * time_years + 0.4 * np.random.randn(n_years)
     measure_four = 1 + 3.45 * time_years + 0.8 * np.random.randn(n_years)
 
-    tick_every_x_year = 2
-    ticks_months = np.arange(0, n_months + 1, tick_every_x_year * 12)
-    tickslabels_years = np.arange(start_year, end_year + 1, tick_every_x_year)
+    # tick_every_x_year = 2
+    # ticks_months = np.arange(0, n_months + 1, tick_every_x_year * 12)
+    # tickslabels_years = np.arange(start_year, end_year + 1, tick_every_x_year)
+    ticks_years = np.arange(start_year, extrapolate_year, 3)
 
     fig = plt.figure(figsize=(6, 6 / 1.618))
     ax_positive = fig.add_axes([0.05, 0.5, 0.85, 0.4])
     ax_negative = fig.add_axes([0.05, 0.1, 0.85, 0.4])
+    ax_background = fig.add_axes([0.05, 0.05, 0.85, 0.4], zorder=-1)
 
+    # ax_positive.patch.set_alpha(0.0)
     ax_positive.yaxis.set_label_position("right")
     ax_positive.yaxis.tick_right()
     ax_positive.spines['left'].set_visible(False)
     ax_positive.spines['top'].set_visible(False)
-    ax_positive.set_xlim([0, n_months])
-    ax_positive.set_ylim([0, 100])
-    ax_positive.set_xticks(ticks_months)
-    ax_positive.set_xticklabels(tickslabels_years)
+    ax_positive.set_xlim([start_year, extrapolate_year])
+    # ax_positive.set_ylim([0, 100])
+    ax_positive.set_xticks(ticks_years)
+    # ax_positive.set_xticklabels(tickslabels_years)
 
     ax_negative.patch.set_alpha(0.0)
     ax_negative.yaxis.tick_right()
@@ -65,26 +73,39 @@ if __name__ == '__main__':
     ax_negative.spines['right'].set_visible(False)
     ax_negative.spines['left'].set_visible(False)
     ax_negative.spines['bottom'].set_visible(False)
-    ax_negative.set_xlim([0, n_months])
-    ax_negative.set_ylim([-5, 10])
+    ax_negative.set_xlim([start_year, extrapolate_year])
+    ax_negative.set_ylim([-2, 4])
+
+    ax_background.set_xlim([start_year, extrapolate_year])
+    ax_background.get_xaxis().set_visible(False)
+    ax_background.get_yaxis().set_visible(False)
+    ax_background.axis('off')
 
     offset = 0
-    ax_positive.fill_between(time_months, offset, offset + sdg_spending_0, color=colors_categories['people'])
+    extrapolate_offset = 0
+    ax_positive.fill_between(start_year + time_months / 12, offset, offset + sdg_spending_0, color=colors_categories['people'])
+    extrapolate_offset += fit_gp_and_predict(start_year + time_months / 12, sdg_spending_0, end_year, extrapolate_year, ax_positive, colors_categories['people'], 0.5, extrapolate_year - end_year)
     offset += sdg_spending_0
-    ax_positive.fill_between(time_months, offset, offset + sdg_spending_1, color=colors_categories['society'])
+    ax_positive.fill_between(start_year + time_months / 12, offset, offset + sdg_spending_1, color=colors_categories['society'])
+    extrapolate_offset += fit_gp_and_predict(start_year + time_months / 12, sdg_spending_1, end_year, extrapolate_year, ax_positive, colors_categories['society'], 0.5, extrapolate_year - end_year, extrapolate_offset)
     offset += sdg_spending_1
-    ax_positive.fill_between(time_months, offset, offset + sdg_spending_2, color=colors_categories['economy'])
+    ax_positive.fill_between(start_year + time_months / 12, offset, offset + sdg_spending_2, color=colors_categories['economy'])
+    extrapolate_offset += fit_gp_and_predict(start_year + time_months / 12, sdg_spending_2, end_year, extrapolate_year, ax_positive, colors_categories['economy'], 0.5, extrapolate_year - end_year, extrapolate_offset)
     offset += sdg_spending_2
-    ax_positive.fill_between(time_months, offset, offset + sdg_spending_3, color=colors_categories['environment'])
+    ax_positive.fill_between(start_year + time_months / 12, offset, offset + sdg_spending_3, color=colors_categories['environment'])
+    extrapolate_offset += fit_gp_and_predict(start_year + time_months / 12, sdg_spending_3, end_year, extrapolate_year, ax_positive, colors_categories['environment'], 0.5, extrapolate_year - end_year, extrapolate_offset)
     offset += sdg_spending_3
-    ax_positive.fill_between(time_months, offset, total_spending, alpha=0.1, color='k', linewidth=0)
-    # ax_positive.plot(time_months, total_spending)
+    ax_positive.fill_between(start_year + time_months / 12, offset, total_spending, alpha=0.1, color='k', linewidth=0)
+    ax_positive.plot(start_year + time_months / 12, total_spending, color='k', alpha=0.8)
+    fit_gp_and_predict(start_year + time_months / 12, total_spending, end_year, extrapolate_year, ax_positive, 'k', 0.5, extrapolate_year - end_year, extrapolate_offset, add_to_offset=False)
 
-    scale = 1.0
-    for year_idx in range(n_years):
-        ax_negative.add_patch(Rectangle(((year_idx + 0.5) * 12, 0), -scale * math.sqrt(measure_one[year_idx]), scale * math.sqrt(measure_one[year_idx]), facecolor=colors_categories['people']))
-        ax_negative.add_patch(Rectangle(((year_idx + 0.5) * 12, 0),  scale * math.sqrt(measure_two[year_idx]), scale * math.sqrt(measure_two[year_idx]), facecolor=colors_categories['society']))
-        ax_negative.add_patch(Rectangle(((year_idx + 0.5) * 12, 0), -scale * math.sqrt(measure_three[year_idx]), -scale * math.sqrt(measure_three[year_idx]), facecolor=colors_categories['economy']))
-        ax_negative.add_patch(Rectangle(((year_idx + 0.5) * 12, 0),  scale * math.sqrt(measure_four[year_idx]), -scale * math.sqrt(measure_four[year_idx]), facecolor=colors_categories['environment']))
+    scale = 0.18
+    step_size = 3
+    for year_idx in range(0, n_years, step_size):
+        ax_negative.add_patch(Rectangle(((start_year + year_idx), 0), -scale * math.sqrt(measure_one[year_idx]), scale * math.sqrt(measure_one[year_idx]), facecolor=colors_categories['people'], clip_on=False))
+        ax_negative.add_patch(Rectangle(((start_year + year_idx), 0),  scale * math.sqrt(measure_two[year_idx]), scale * math.sqrt(measure_two[year_idx]), facecolor=colors_categories['society'], clip_on=False))
+        ax_negative.add_patch(Rectangle(((start_year + year_idx), 0), -scale * math.sqrt(measure_three[year_idx]), -scale * math.sqrt(measure_three[year_idx]), facecolor=colors_categories['economy'], clip_on=False))
+        ax_negative.add_patch(Rectangle(((start_year + year_idx), 0),  scale * math.sqrt(measure_four[year_idx]), -scale * math.sqrt(measure_four[year_idx]), facecolor=colors_categories['environment'], clip_on=False))
+        ax_background.plot((start_year + year_idx, start_year + year_idx), (0, 1), color='k', linewidth=2, alpha=0.2, clip_on=False, zorder=-1)
 
     fig.savefig('plot.pdf')
--- utils/fit_GP_and_predict.py
+++ utils/fit_GP_and_predict.py
@@ -15,25 +15,33 @@ Input:
 @author: horvat
 """
 
+import numpy as np
 from sklearn.datasets import make_friedman2
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel
 
-def plt_errorbar(xplt,yplt,yerr,label=None,lw=2,c='steelblue',marker='',alpha=0.3,ls=None):
-    ax.plot(xplt,yplt,lw=lw,c=c,marker=marker,ls=ls,label=label)
-    ax.fill_between(xplt,yplt-yerr,yplt+yerr,color=c,alpha=alpha)
+def plt_errorbar(ax, xplt,yplt,yerr,label=None,lw=2,c='steelblue',marker='',alpha=0.3,ls=None,offset=0,zorder=None,add_to_offset=True):
+    # ax.plot(xplt,offset + yplt,lw=lw,c=c,marker=marker,ls=ls,label=label, alpha=alpha)
+    # ax.fill_between(xplt,offset + yplt-yerr,yplt+yerr,color=c,alpha=alpha)
+    if add_to_offset:
+        # ax.plot(xplt, offset + yplt, color=c, alpha=alpha,zorder=-1)
+        ax.fill_between(xplt, offset, offset + yplt, color=c, alpha=alpha, linewidth=0, zorder=-1)
+    else:
+        # ax.plot(xplt, yplt, color=c, alpha=alpha, zorder=-1)
+        ax.fill_between(xplt, offset, yplt, color=c, alpha=alpha, linewidth=0, zorder=-1)
 
-def fit_gp_and_predict(time,y,t_start,t_end,ax,fill_color,opa):
+def fit_gp_and_predict(time,y,t_start,t_end,ax,fill_color,opa,dt,offset=0,zorder=None,add_to_offset=True):
     # fit GP
     kernel = DotProduct() + WhiteKernel()
     gpr = GaussianProcessRegressor(kernel=kernel,
-                               random_state=0).fit(dt.reshape([time.shape[0],1]), y)
+                               random_state=0).fit(time.reshape([time.shape[0],1]), y)
     #create prediction
-    t_pred = np.linspace(t_start,t_end,dt).reshape([dt,1])
-    y_pred = gpr.sample_y(x_pred,n_samples=dt,random_state=0)
-    y_mean = np.mean(y_pred,axis=0)
-    y_err = np.std(y_pred,axis=0)
+    t_pred = np.linspace(t_start-0.1,t_end,dt).reshape([dt,1])
+    y_pred = gpr.sample_y(t_pred,n_samples=dt,random_state=0)
+    y_mean = np.mean(y_pred,axis=1)
+    y_mean = np.clip(y_mean, 0.0, np.inf)
+    y_err = np.std(y_pred,axis=1)
     y_mean[0] = y[-1]  #smooth transition
     
-    plt_errorbar(np.linspace(t_start,t_end,dt), y_mean, y_err,c=fill_color,alpha=opa) 
-
+    plt_errorbar(ax, t_pred.flatten(), y_mean, y_err,c=fill_color,alpha=opa,offset=offset,zorder=zorder,add_to_offset=add_to_offset)
+    return y_mean
